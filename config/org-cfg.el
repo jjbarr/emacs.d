ad3b766662aedb631f3ecb0fdb5fc61fb5a3fd35
Joshua Barrett <jjbarr.public@gmail.com>
2022-02-02 15:55:16 -0500

    added graphviz prereqs

--- config/org-cfg.el
+++ config/org-cfg.el
@@ -2,6 +2,9 @@
 ;;; Commentary:
 ;; I'm gonna fill this out later
 ;;; Code:
+
+(use-package graphviz-dot-mode :ensure t
+  :config (setq graphviz-dot-indent-width 4))
 (use-package org :ensure t
   :pin "org"
   :config
@@ -9,6 +12,9 @@
       (setq org-directory (substitute-in-file-name "$HOMEPATH/org")))
   (setq org-agenda-files (list org-directory))
   (setq org-default-notes-file (concat org-directory "/todo.org"))
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((dot . t)))
   :mode ("\\.org\\'" . org-mode)
   :bind (("C-c l" . org-store-link)
          ("C-c a" . org-agenda)
