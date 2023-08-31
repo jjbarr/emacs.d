cbf4db965582e3874dfe2b811ac43e98878eb3ed
Joshua Barrett <jjbarr.public@gmail.com>
2023-08-31 18:16:54 -0400

    eliminated keychain (no longer in use)

--- config/site-unix.el
+++ config/site-unix.el
@@ -1,8 +1,4 @@
 ;;; site-unix -- settings that break on the non-unices
 ;;; Commentary:
 ;;; Code:
-
-(use-package keychain-environment :straight t
-  :config (keychain-refresh-environment))
-
 ;;; site-unix.el ends here
