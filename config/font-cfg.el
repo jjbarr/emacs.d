e2b1023bb4d0ef8bc9becfb18fa7cf0cd1ba7eb1
Joshua Barrett <jjbarr.public@gmail.com>
2023-09-01 20:27:19 -0400

    erased font-cfg, it lives in general now

--- config/font-cfg.el
+++
@@ -1,9 +0,0 @@
-;;; font-cfg.el -- configure fonts
-;;; Commentary:
-;; none.
-;;; Code:
-(apply-if-gui
- (lambda ()
-   (set-face-attribute 'default nil :font "Terminus (TTF)" :height 120)))
-;;; font-cfg.el ends here
-
