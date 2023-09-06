054ae4de23150c686969e3b1a1193157590ef032
Joshua Barrett <jjbarr.public@gmail.com>
2023-09-05 23:13:40 -0400

    adjusted corfu autocomplete because it was slightly too much

--- config/corfu-cfg.el
+++ config/corfu-cfg.el
@@ -6,8 +6,8 @@
   :config
   (setq corfu-auto t
         corfu-quit-no-match 'separator
-        corfu-auto-delay 0
-        corfu-auto-prefix 3)
+        corfu-auto-delay 0.5
+        corfu-auto-prefix 4)
   (add-hook 'text-mode-hook (lambda () (setq-local corfu-auto nil)))
   (defun corfu-move-to-minibuffer ()
     (interactive)
